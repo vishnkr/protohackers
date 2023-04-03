4c9436653be725774d07051b82fffc0ace386a2f
Vishwa Shankar <vishwas1020@gmail.com>
2023-04-03 01:24:34 -0700

    add logger

--- smoke-test/src/main.rs
+++ smoke-test/src/main.rs
@@ -3,12 +3,14 @@ use tokio::{
     io::{AsyncReadExt,AsyncWriteExt}
 };
 
-const SERVER_ADDR : &str = "127.0.0.1:1234";
+const SERVER_ADDR : &str = "0.0.0.0:1234";
 
 #[tokio::main]
 async fn main() {
     println!("starting server {}",SERVER_ADDR);
+    let mut client = 0;
     let listener = TcpListener::bind(SERVER_ADDR).await.unwrap();
+    log::info!("Listening on {}", listener.local_addr().unwrap());
     loop {
         let ( mut stream, _ ) = listener.accept().await.unwrap();
         tokio::spawn(async move{
@@ -20,22 +22,20 @@ async fn main() {
 async fn handle_conn(mut stream:TcpStream){
     //read
     let mut buffer = [0;1024];
-    let len = match stream.read(&mut buffer).await{
-        Ok(n) if n==0 => return,
-        Ok(n) =>n,
-        Err(e) =>{
-            println!("failed to read from socket: {}",e);
-            return;
-        }
-    };
-    let message = String::from_utf8_lossy(&buffer[..len]);
-    println!("Received: {}",message);
-
-    //write
-    
-    if let Err(e) = stream.write_all(&buffer[..len]).await{
-        println!("failed to write to socket:{}",e);
-        return;
+    loop{
+        match stream.read(&mut buffer).await{
+            Ok(0) => break,
+            Ok(n) =>{
+                log::debug!("read {} bytes",n);
+                if let Err(e) = stream.write_all(&buffer[..n]).await{
+                    log::error!("failed to write to socket:{}",e);
+                }
+                break;
+            },
+            Err(e) =>{
+                log::error!("failed to read from socket: {}",e);
+                return;
+            }
+        };
     }
-    println!("sent {}",message);
 }   
