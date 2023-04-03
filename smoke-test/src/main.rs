fabf18f5341e7591a3fbdf6c2ef08f12f1979d07
Vishwa Shankar <vishwas1020@gmail.com>
2023-04-03 02:02:30 -0700

    remove extra break on successful read

--- smoke-test/src/main.rs
+++ smoke-test/src/main.rs
@@ -1,3 +1,4 @@
+
 use tokio::{
     net::{TcpListener, TcpStream},
     io::{AsyncReadExt,AsyncWriteExt}
@@ -7,21 +8,33 @@ const SERVER_ADDR : &str = "0.0.0.0:1234";
 
 #[tokio::main]
 async fn main() {
+    env_logger::init();
     println!("starting server {}",SERVER_ADDR);
-    let mut client = 0;
     let listener = TcpListener::bind(SERVER_ADDR).await.unwrap();
     log::info!("Listening on {}", listener.local_addr().unwrap());
     loop {
-        let ( mut stream, _ ) = listener.accept().await.unwrap();
-        tokio::spawn(async move{
-            handle_conn(stream).await;
-        });
+
+        match listener.accept().await {
+            Err(e) => {
+                log::error!("Error accepting connection: {}", &e);
+            },
+            Ok((stream,addr))=>{
+                log::debug!(
+                    "Accepted connection from {}", addr
+                );
+                tokio::spawn(async move{
+                    handle_conn(stream).await
+                });
+            }
+        }
+        
     }
+
 }
 
 async fn handle_conn(mut stream:TcpStream){
     //read
-    let mut buffer = [0;1024];
+    let mut buffer = [0u8;1024];
     loop{
         match stream.read(&mut buffer).await{
             Ok(0) => break,
@@ -29,12 +42,13 @@ async fn handle_conn(mut stream:TcpStream){
                 log::debug!("read {} bytes",n);
                 if let Err(e) = stream.write_all(&buffer[..n]).await{
                     log::error!("failed to write to socket:{}",e);
+                    break;
                 }
-                break;
+                log::debug!("wrote {} bytes",n);
             },
             Err(e) =>{
-                log::error!("failed to read from socket: {}",e);
-                return;
+                    log::error!("failed to read from socket: {}",e);
+                    break;
             }
         };
     }
